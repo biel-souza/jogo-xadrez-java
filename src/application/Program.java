301dc9866d6b0cbd6dc2954a536f35277039642f
Gabriel Eduardo de Souza <gabriel.souza@senior.com.br>
2024-10-17 19:23:38 -0300

    add class position

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package src.application;
+
+import src.board.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+
+    }
+}
