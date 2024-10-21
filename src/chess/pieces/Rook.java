73629a018db96ebad2ec17dbba8d9f93a27fd2f3
Gabriel Eduardo de Souza <gabriel.souza@senior.com.br>
2024-10-21 17:27:16 -0300

    add initial peaces

---
+++ src/chess/pieces/Rook.java
@@ -0,0 +1,17 @@
+package src.chess.pieces;
+
+import src.chess.ChessPiece;
+import src.chess.Color;
+import src.game.Board;
+
+public class Rook extends ChessPiece {
+
+    public Rook(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "♜";
+    }
+}
